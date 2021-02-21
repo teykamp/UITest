22bca97fb3368d07b60ad9beb1b54f289d9bbab0
Thomas Eykamp <embergameslp@gmail.com>
2021-02-20 21:44:39 -0500

    Vue vers. Update

--- uitest/src/main.js
+++ uitest/src/main.js
@@ -1,8 +1,15 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
+import Vue from 'vue'
 import './plugins/bootstrap-vue'
-import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import vuetify from './plugins/vuetify';
 
-createApp(App).use(store).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
