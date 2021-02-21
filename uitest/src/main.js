d238ea3f140f5b9df1cd83da961f5c0f079b809a
Thomas Eykamp <embergameslp@gmail.com>
2021-02-20 21:11:02 -0500

    Vue Install + Plugins

---
+++ uitest/src/main.js
@@ -0,0 +1,8 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import './plugins/bootstrap-vue'
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store'
+
+createApp(App).use(store).mount('#app')
